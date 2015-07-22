e0aad16e0287d23c22a95ca0831d6b22c459ff20
John Cordeiro <john.dalton@ilhasoft.com.br>
2015-07-22 18:27:27 -0300

    Add status bar designer tool

---
+++ src/main/java/br/com/ilhasoft/support/tool/StatusBarDesigner.java
@@ -0,0 +1,24 @@
+package br.com.ilhasoft.support.tool;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.support.annotation.ColorRes;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by johncordeiro on 7/22/15.
+ */
+public class StatusBarDesigner {
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void setStatusBarColor(Activity activity, @ColorRes int color) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(activity.getResources().getColor(color));
+        }
+    }
+
+}
