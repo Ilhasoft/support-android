bbd3ccd24b8bd315383e60e9037146ee18f2286a
John Cordeiro <john.dalton@ilhasoft.com.br>
2015-08-11 17:00:07 -0300

    Add the capability to change status bar color by id and transparent

--- src/main/java/br/com/ilhasoft/support/tool/StatusBarDesigner.java
+++ src/main/java/br/com/ilhasoft/support/tool/StatusBarDesigner.java
@@ -21,4 +21,21 @@ public class StatusBarDesigner {
         }
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void setStatusBarColorById(Activity activity, @ColorRes int color) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(activity.getResources().getColor(color));
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public void setStatusBarTransparent(Activity activity) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = activity.getWindow();
+            window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+    }
+
 }
