ed4aeab69913360aa9a17520932305822900180b
John Cordeiro <john.dalton@ilhasoft.com.br>
2016-09-08 17:52:35 -0300

    Add max date support

--- src/main/java/br/com/ilhasoft/support/widget/DatePickerFragment.java
+++ src/main/java/br/com/ilhasoft/support/widget/DatePickerFragment.java
@@ -2,18 +2,35 @@ package br.com.ilhasoft.support.widget;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
 
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by johncordeiro on 17/07/15.
  */
 public class DatePickerFragment extends DialogFragment {
 
+    private static final String EXTRA_MAX_DATE = "maxDate";
+
+    private static final int INVALID_VALUD = -1;
     private DatePickerDialog.OnDateSetListener onDateSetListener;
+
+    private long maxDate;
+
+    public static DatePickerFragment newInstance(Date maxDate) {
+        Bundle args = new Bundle();
+        args.putLong(EXTRA_MAX_DATE, maxDate.getTime());
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @NonNull
     @Override
@@ -22,8 +39,18 @@ public class DatePickerFragment extends DialogFragment {
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
         int day = calendar.get(Calendar.DAY_OF_MONTH);
+        maxDate = getArguments().getLong(EXTRA_MAX_DATE, INVALID_VALUD);
 
-        return new DatePickerDialog(getActivity(), onDateSetListener, year, month, day);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), onDateSetListener, year, month, day);
+        setMaxDate(datePickerDialog);
+        return datePickerDialog;
+    }
+
+    private void setMaxDate(DatePickerDialog datePickerDialog) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB && maxDate != INVALID_VALUD) {
+            DatePicker datePicker = datePickerDialog.getDatePicker();
+            datePicker.setMaxDate(Calendar.getInstance().getTimeInMillis());
+        }
     }
 
     public void setOnDateSetListener(DatePickerDialog.OnDateSetListener onDateSetListener) {
